76e78db17de0e0eac27534837eab4a23aca6603a
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-28 10:13:10 -0500

    Replace LibraMarkdown with AgParsedown

--- config/application.config.php
+++ config/application.config.php
@@ -3,7 +3,7 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
-        'LibraMarkdown',
+        'AgParsedown',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
