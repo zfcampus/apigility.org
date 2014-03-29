9d03ae3d0ade1bef08492b61d248d542bcf785b3
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-29 11:28:46 -0500

    Added AssetManager to repo
    - Added AssetManager dep
    - Added AssetManager and ApigilityDocumentation to module list

--- config/application.config.php
+++ config/application.config.php
@@ -3,7 +3,9 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'ApigilityDocumentation',
         'AgParsedown',
+        'AssetManager',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
