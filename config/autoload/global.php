4d21ae50d0eea9c41e0a14059521ee2ede8f82e3
Matthew Weier O'Phinney <matthew@zend.com>
2016-03-16 13:34:46 -0500

    1.3.3 release

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -13,11 +13,11 @@
 
 return array(
 	'apigility' => array(
-		'version' => '1.3.1'
+		'version' => '1.3.3'
 	),
     'links' => array(
-		'zip' => 'https://github.com/zfcampus/zf-apigility-skeleton/releases/download/1.3.1/zf-apigility-skeleton-1.3.1.zip',
-    	'tgz' => 'https://github.com/zfcampus/zf-apigility-skeleton/releases/download/1.3.1/zf-apigility-skeleton-1.3.1.tgz',
+		'zip' => 'https://github.com/zfcampus/zf-apigility-skeleton/releases/download/1.3.3/zf-apigility-skeleton-1.3.3.zip',
+    	'tgz' => 'https://github.com/zfcampus/zf-apigility-skeleton/releases/download/1.3.3/zf-apigility-skeleton-1.3.3.tgz',
     	'forkme' => 'https://github.com/zfcampus'
 	)
 );
